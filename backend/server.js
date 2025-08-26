c7884f18d65440d78e7fb5ecae0bd9476c813637
reehan7086 <63193615+reehan7086@users.noreply.github.com>
2025-08-26 18:48:24 +0000

    server frontend from backend

--- backend/server.js
+++ backend/server.js
@@ -220,9 +220,12 @@ fastify.post('/api/generate-vibe-card', async (request, reply) => {
     if (process.env.NODE_ENV === 'production') {
       await fastify.register(fastifyStatic, {
         root: path.join(__dirname, '../frontend/dist'),
-        prefix: '/static/',
+        prefix: '/',
         decorateReply: false,
       });
+        fastify.get('/*', (request, reply) => {
+    reply.sendFile('index.html');
+  });
     }
 
     // 404 handler
